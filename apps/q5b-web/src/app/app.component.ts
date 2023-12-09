bb461508c4dbbd52df0ed86f8baa8068dfc2a927
Reinaldo Usadani <reinaldo.usadani@commsult.id>
2023-12-10 01:10:54 +0700

    Formatted

--- apps/q5b-web/src/app/app.component.ts
+++ apps/q5b-web/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { isOdd } from 'is-odd';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'q5b-web';
+  title = `q5b-web, ${isOdd(2) ? 'true' : 'false'}`;
 }
