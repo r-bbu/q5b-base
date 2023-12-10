3d488abef0e79d0cb3a3338695bf60d3b3bafa1c
Reinaldo Usadani <reinaldo.usadani@commsult.id>
2023-12-10 18:54:39 +0700

    Fixed wrong folder placement

--- apps/q5b-core/src/app/app.module.ts
+++ apps/q5b-core/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './modules/cats/cats.controller';
-import { DogsModule } from './modules/dogs/dogs/dogs.module';
+import { DogsModule } from './modules/dogs/dogs.module';
 
 @Module({
   imports: [DogsModule],
