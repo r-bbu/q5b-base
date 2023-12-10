90c694047c3647a0949453bd6b14ce2b74a0dffd
Reinaldo Usadani <reinaldo.usadani@commsult.id>
2023-12-10 18:51:34 +0700

    holy crap, nest g resource is OP

--- apps/q5b-core/src/app/app.module.ts
+++ apps/q5b-core/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './modules/cats/cats.controller';
+import { DogsModule } from './modules/dogs/dogs/dogs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [DogsModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
