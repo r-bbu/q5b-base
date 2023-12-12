7ca7297a490ec4812c76418fe6002c15affb065f
Reinaldo Usadani <reinaldo.usadani@commsult.id>
2023-12-12 23:12:01 +0700

    Created a local strategy

--- apps/q5b-core/src/app/modules/auth/auth.module.ts
+++ apps/q5b-core/src/app/modules/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [UserModule],
+  providers: [AuthService, LocalStrategy],
+  imports: [UserModule, PassportModule],
 })
 export class AuthModule {}
