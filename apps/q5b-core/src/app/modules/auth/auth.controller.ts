43a475698cfc696de28c540efac3503854dbd7da
Reinaldo Usadani <reinaldo.usadani@commsult.id>
2023-12-12 23:17:19 +0700

    Holy shit this works

--- apps/q5b-core/src/app/modules/auth/auth.controller.ts
+++ apps/q5b-core/src/app/modules/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signin/local')
+  @UseGuards(AuthGuard('local'))
+  async login(@Request() req) {
+    return req.user;
+  }
 }
