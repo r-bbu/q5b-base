dfb47fd939b36957e1dd5199117ff200377bb635
Reinaldo Usadani <reinaldo.usadani@commsult.id>
2023-12-12 23:46:41 +0700

    Moved local auth guard for max SCALABILITY

--- apps/q5b-core/src/app/modules/auth/auth.controller.ts
+++ apps/q5b-core/src/app/modules/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin/local')
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   async login(@Request() req) {
     return req.user;
   }
